86efddc3328294693bd552f6ee9a1eb66753c26a
Hasnathasan <hasnatoooooooo@gmail.com>
2023-03-03 03:49:14 +0600

    modal added

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ const showTools = (data) =>{
                             <p>${tool.published_in}</p>
                         </div>
                     </div>
-                    <label for="my-modal-5" onclick="toolDetails(tool.id)" class="btn bg-orrange-100 text-orrange rounded-3xl w-11 h-11 border-0 p-1"><i class="fa-solid fa-arrow-right"></i></label>
+                    <label for="my-modal-5" onclick="toolDetails('${tool.id}')" class="btn bg-misty-100 text-misty rounded-3xl w-11 h-11 border-0 p-1"><i class="fa-solid fa-arrow-right"></i></label>
                     
                 </div>
                 
@@ -42,5 +42,25 @@ const showTools = (data) =>{
 
 
 const toolDetails = (id) => {
-    
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showDetailsInModal(data.data));
+}
+
+const showDetailsInModal = data => {
+    console.log(data)
+    document.getElementById('modalTitle').innerText = `${data.description}`;
+    document.getElementById('pricing1').innerText = `${data.pricing[0].price ? data.pricing[0].price : 'Free of Cost'} ${data.pricing[0].plan}`;
+    document.getElementById('pricing2').innerText = `${data.pricing[1].price ? data.pricing[1].price : 'Free of Cost'} ${data.pricing[1].plan}`;
+    document.getElementById('pricing3').innerText = `${data.pricing[2].price ? data.pricing[2].price : 'Free of Cost'} ${data.pricing[2].plan}`;
+    document.getElementById('fearure1').innerText = `. ${data.features[1].feature_name}`;
+    document.getElementById('fearure2').innerText = `. ${data.features[2].feature_name}`;
+    document.getElementById('fearure3').innerText = `. ${data.features[3].feature_name}`;
+    document.getElementById('in1').innerText = `. ${data.integrations[0]}`;
+    document.getElementById('in2').innerText = `. ${data.integrations[1]}`;
+    document.getElementById('in3').innerText = `. ${data.integrations[2]}`;
+    document.getElementById('modal-img').setAttribute('src', `${data.image_link[0]}`);
+    document.getElementById('modal-input-example').innerText = `${data.input_output_examples[0].input}`;
+    document.getElementById('modal-output-example').innerText = `${data.input_output_examples[0].output}`;
 }
