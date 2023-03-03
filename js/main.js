bdbd65c6a587094e2f7ba282679418ece1574f06
Hasnathasan <hasnatoooooooo@gmail.com>
2023-03-03 19:48:36 +0600

    modal modified

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-const fetchApiTools = async() =>{
+const fetchApiTools = async (slice) => {
     const url = 'https://openapi.programming-hero.com/api/ai/tools';
     const res = await fetch(url);
     const data = await res.json();
-    showTools(data.data.tools)
+    showTools(data.data.tools, slice)
 }
 
-const showTools = (data) =>{
-    console.log(data);
+const showTools = (data, slice) => {
     const toolsContainer = document.getElementById('tools-container');
+    toolsContainer.innerHTML = '';
+    const seeMoreBtn = document.getElementById('see-more');
+    if (slice && data.length > 6) {
+        data = data.slice(0, 6);
+        seeMoreBtn.classList.remove('hidden');
+    }
+    else {
+        seeMoreBtn.classList.add('hidden');
+    };
     data.forEach(tool => {
         console.log(tool);
         toolsContainer.innerHTML += `
@@ -27,13 +35,13 @@ const showTools = (data) =>{
                             <p>${tool.published_in}</p>
                         </div>
                     </div>
-                    <label for="my-modal-5" onclick="toolDetails('${tool.id}')" class="btn bg-misty-100 text-misty rounded-3xl w-11 h-11 border-0 p-1"><i class="fa-solid fa-arrow-right"></i></label>
+                    <label for="my-modal-5" onclick="toolDetails('${tool.id}')" class="btn bg-misty100 text-misty rounded-3xl w-11 h-11 border-0 p-1"><i class="fa-solid fa-arrow-right"></i></label>
                     
                 </div>
                 
             </div>
         </div>
-        `
+        `;
     })
 }
 
@@ -44,23 +52,34 @@ const showTools = (data) =>{
 const toolDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => showDetailsInModal(data.data));
+        .then(res => res.json())
+        .then(data => showDetailsInModal(data.data));
+}
+const showDetailsInModal = data => {
+    console.log(data);
+    document.getElementById('modalTitle').innerText = `${data.description}`;
+    document.getElementById('pricing1').innerText = `${data.pricing ? data.pricing[0].price : 'Free of Cost'} ${data.pricing ? data.pricing[0].plan : ''}`;
+    document.getElementById('pricing2').innerText = `${data.pricing ? data.pricing[1].price : 'Free of Cost'} ${data.pricing ? data.pricing[1].plan : ''}`;
+    document.getElementById('pricing3').innerText = `${data.pricing ? data.pricing[2].price : 'Free of Cost'} ${data.pricing ? data.pricing[2].plan : ''}`;
+    document.getElementById('fearure1').innerText = `${data.features ? data.features[1].feature_name : 'Not Available'}`;
+    document.getElementById('fearure2').innerText = `${data.features ? data.features[2].feature_name : 'Not Available'}`;
+    document.getElementById('fearure3').innerText = `${data.features ? data.features[3].feature_name : 'Not Available'}`;
+    if(Array.isArray(data.integrations) === true){
+        document.getElementById('in1').innerText = `${data.integrations ? data.integrations[0] : 'Not Available'}`;
+        document.getElementById('in2').innerText = `${data.integrations[1] ? data.integrations[1] : 'Not Available'}`;
+        document.getElementById('in3').innerText = `${data.integrations[2] ? data.integrations[2] : 'Not Available'}`;
+    }
+    else{
+        document.getElementById('in1').innerText = 'Not Available';
+        document.getElementById('in2').innerText = 'Not Available';
+        document.getElementById('in3').innerText = 'Not Available';
+    }
+    
+    document.getElementById('modal-img').setAttribute('src', `${data.image_link[0]}`);
+    document.getElementById('modal-input-example').innerHTML = `${data.input_output_examples ? data.input_output_examples[0].input : 'Can you give any Example'}`;
+    document.getElementById('modal-output-example').innerHTML = `${data.input_output_examples ? data.input_output_examples[0].output : 'No! Not yot! Take a break!!!'}`;
 }
 
-const showDetailsInModal = data => {
-    console.log(data)
-    document.getElementById('modalTitle').innerText = `${data.description}`;
-    document.getElementById('pricing1').innerText = `${data.pricing[0].price ? data.pricing[0].price : 'Free of Cost'} ${data.pricing[0].plan}`;
-    document.getElementById('pricing2').innerText = `${data.pricing[1].price ? data.pricing[1].price : 'Free of Cost'} ${data.pricing[1].plan}`;
-    document.getElementById('pricing3').innerText = `${data.pricing[2].price ? data.pricing[2].price : 'Free of Cost'} ${data.pricing[2].plan}`;
-    document.getElementById('fearure1').innerText = `. ${data.features[1].feature_name}`;
-    document.getElementById('fearure2').innerText = `. ${data.features[2].feature_name}`;
-    document.getElementById('fearure3').innerText = `. ${data.features[3].feature_name}`;
-    document.getElementById('in1').innerText = `. ${data.integrations[0]}`;
-    document.getElementById('in2').innerText = `. ${data.integrations[1]}`;
-    document.getElementById('in3').innerText = `. ${data.integrations[2]}`;
-    document.getElementById('modal-img').setAttribute('src', `${data.image_link[0]}`);
-    document.getElementById('modal-input-example').innerText = `${data.input_output_examples[0].input}`;
-    document.getElementById('modal-output-example').innerText = `${data.input_output_examples[0].output}`;
+const seeMore = () => {
+    fetchApiTools();
 }
